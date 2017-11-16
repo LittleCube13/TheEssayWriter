9cb64522936ae06a6c051ac8569829a601a96799
LittlestCube <samuelcwhidden@gmail.com>
2017-11-16 02:54:15 -0500

    disabled stripPunctuation(), at least for now

--- outputwriter.java
+++ outputwriter.java
@@ -51,21 +51,21 @@ class outputwriter extends EssayWriter {
 	public static String createEssay() {
 		String essay = "";
 		try {
-			String thesis1 = stripPunctuation(thesis.getText());
-			String parapoint1 = stripPunctuation(point1.getText());
-			String parapoint2 = stripPunctuation(point2.getText());
-			String parapoint3 = stripPunctuation(point3.getText());
-			String ex1 = stripPunctuation(exp1.getText());
-			String ex2 = stripPunctuation(exp2.getText());
-			String ex3 = stripPunctuation(exp3.getText());
-			String de1 = stripPunctuation(def1.getText());
-			String de2 = stripPunctuation(def2.getText());
-			String de3 = stripPunctuation(def3.getText());
-			String il1 = stripPunctuation(ill1.getText());
-			String il2 = stripPunctuation(ill2.getText());
-			String il3 = stripPunctuation(ill3.getText());
-			String intro1 = stripPunctuation(intro.getText());
-			String conclu1 = stripPunctuation(conclu.getText());
+			String thesis1 = thesis.getText();
+			String parapoint1 = point1.getText();
+			String parapoint2 = point2.getText();
+			String parapoint3 = point3.getText();
+			String ex1 = exp1.getText();
+			String ex2 = exp2.getText();
+			String ex3 = exp3.getText();
+			String de1 = def1.getText();
+			String de2 = def2.getText();
+			String de3 = def3.getText();
+			String il1 = ill1.getText();
+			String il2 = ill2.getText();
+			String il3 = ill3.getText();
+			String intro1 = intro.getText();
+			String conclu1 = conclu.getText();
 			essay = "\t" + period(upper(intro1)) + " " + upper(thesis1) + " because " + lower(comma(parapoint1)) + " " + lower(comma(parapoint2)) + " and finally, " + lower(period(parapoint3)) + "\n\n\t" + "First, " + lower(thesis1) + " because " + lower(period(parapoint1)) + " " + period(upper(ex1)) + " " + period(upper(de1)) + " " + period(upper(il1)) + "\n\n\t" + "Moreover, " + lower(thesis1) + " because " + lower(period(parapoint2)) + " " + period(upper(ex2)) + " " + period(upper(de2)) + " " + period(upper(il2)) + "\n\n\t" + "Furthermore, " + lower(thesis1) + " because " + lower(period(parapoint3)) + " " + period(upper(ex3)) + " " + period(upper(de3)) + " " + period(upper(il3)) + "\n\n\t" + upper(thesis1) + " because " + lower(comma(parapoint1)) + " " + lower(comma(parapoint2)) + " and finally, " + lower(period(parapoint3)) + " " + period(upper(conclu1));
 		} catch (Exception e) { System.err.println("Crap happened."); }
 	return essay;
